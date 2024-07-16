ddd03dd34f6324825cf807c23b2c8e743e1c6a6d
Pavan Gudiwada <25551553+pavangudiwada@users.noreply.github.com>
2024-07-16 21:42:02 +0530

    more changes

--- code/meme-generator/main.py
+++ code/meme-generator/main.py
@@ -3,20 +3,21 @@ import json
 import requests
 import random
 import platform
+import os
 
 app = Flask(__name__)
 
-
 @app.route("/", methods=["POST", "GET"])
 def generator():
-    sub_reddit = ["Kubernetes", "dockermemes", "ProgrammerHumor"] #subreddits to get memes from
-    url = "https://meme-api.com/gimme/" + random.choice(sub_reddit) 
+    sub_reddit = os.getenv("SUBREDDITS", "Kubernetes,dockermemes,ProgrammerHumor").split(",")  # Get subreddits from env variable
+    url = os.getenv("MEME_API_URL", "https://meme-api.com/gimme/") + random.choice(sub_reddit)  # Get URL from env variable
     meme_data = json.loads(requests.get(url).text)
-    meme_image = meme_data["preview"][-1] #get a medium size meme image
+    print(meme_data)
+    meme_image = meme_data["preview"][-1]  # get a medium size meme image
 
-    host = platform.uname() #details about the host
+    host = platform.uname()  # details about the host
     
     return render_template("index.html", meme_image=meme_image, host_name=host.node, host_type=host.system, host_arch=host.machine)
 
 if __name__ == "__main__":
-    app.run(port=5000, host="0.0.0.0")
+    app.run(port=5000, host="0.0.0.0", debug=True)
